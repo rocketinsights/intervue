10139a7552a3e7d93574707b2230cc893a715742
Brandon Aaskov <brandon@rocketinsights.com>
2018-08-23 13:51:46 -0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo />
+      <h1 class="title">Intervue</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+import AppLogo from '@/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  },
+  async data () {
+    const { data } = await axios.get('http://huntingmeat.com/api/search?text=optics')
+    console.log(data)
+    // initialize state here
+    return {}
+  },
+  computed: {},
+  methods: {},
+  mounted () {}
+}
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
+
